f9cb3694f64b508b1600a9e0394cb0e496237609
divyanshusahu <dsahu1997@gmail.com>
2020-10-02 00:33:36 +0530

    custom document and app added

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import Head from "next/head";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import "../styles/tailwind.css";
+
+function CustomApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="robots" content="index,follow" />
+        <meta name="googlebot" content="index,follow" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default CustomApp;
